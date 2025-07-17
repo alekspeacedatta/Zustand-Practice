4fad90a8ba33b948c819c1b790d01f875c9dd612
aleksandre <faleqs33@gmail.com>
2025-07-17 21:26:32 +0400

    trying to create product list for each user

--- src/components/SelectedUser.tsx
+++ src/components/SelectedUser.tsx
@@ -1,7 +1,18 @@
+import { useEffect, useState } from "react";
+import { type ProductType } from "../Types/Type";
 import { useUserStore } from "../stores/useUserStore"
+import { useProductStore } from "../stores/useProductStore";
 const SelectedUser = () => {
 
     const selectedUser = useUserStore(state => state.selectedUser);
+    const products = useProductStore(state => state.products);
+    const deleteAllProducts  = useProductStore(state => state.deleteAllProduct)
+
+    const [ product, setProduct ] = useState<ProductType | {}>({});
+
+    useEffect(() => {
+        deleteAllProducts();
+    }, [selectedUser])
 
     return (
         <div className="selected-user">
